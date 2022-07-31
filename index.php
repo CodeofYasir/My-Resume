512318bbb05402c2c30a6ef3296908719ea4c275
yasirakram <yasir311997@gmail.com>
2022-07-31 19:50:02 +0500

    ch

--- index.php
+++ index.php
@@ -107,134 +107,128 @@
             </div>
         </div>
         <div class="right_side">
-            <div class="about">
-                <h2 class="title2">Objective</h2>
-                <p>An energetic and imaginative young web developer who
-                    can work alongside other talented IT professionals in
-                    creating websites to the very highest standards.
-                    Dedicated to driving innovation with the ability to follow
-                    industry and technological trends and facilitating early
-                    adaption of innovations. To be able to work for an
-                    encouraging and stable company that will assist me in
-                    developing, improving and obtaining the necessary skills
-                    in order to become the best engineer possible.</p>
-            </div>
-            <div class="about">
-                <h2 class="title2">Experience</h2>
-                <div class="box">
-                    <!-- <div class="year_company">
-                        <h5>2021 - present</h5>
-                        <h5>Company Name</h5>
-                    </div> -->
-                    <div class="text">
-                        <h4>Web developer(Internship)</h4>
-                        <p>Hybrid developers, Lahore</p>
+                <div class="about">
+                    <h2 class="title2">Objective</h2>
+                    <p>An energetic and imaginative young web developer who
+                        can work alongside other talented IT professionals in
+                        creating websites to the very highest standards.
+                        Dedicated to driving innovation with the ability to follow
+                        industry and technological trends and facilitating early
+                        adaption of innovations. To be able to work for an
+                        encouraging and stable company that will assist me in
+                        developing, improving and obtaining the necessary skills
+                        in order to become the best engineer possible.</p>
+                </div>
+                <div class="about">
+                    <h2 class="title2">Experience</h2>
+                    <div class="box">
+                        <!-- <div class="year_company">
+                            <h5>2021 - present</h5>
+                            <h5>Company Name</h5>
+                        </div> -->
+                        <div class="text">
+                            <h4>Web developer(Internship)</h4>
+                            <p>Hybrid developers, Lahore</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="text">
+                            <h4>Self Employed</h4>
+                            <p>I have completed some projects on my own. As like Web Development project, after that i am
+                                also participated in the Final Year Project which we are completed as a team, and some other
+                                projects as well which are add in the projects menu already.</p>
+                        </div>
+                    </div>
+                    <h2 class="title2">Personal Projects</h2>
+                    <div class="box">
+                        <div class="text">
+                            <h4>CS Software Providere </h4>
+                            <p>It is a Client base project. I use Html, CSS, Js front-end. Php language use in back-end. Phpmyadmin Xamp local server. All Crud functions used in it.</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="text">
+                            <h4>Inventory Managment System (FYP)</h4>
+                            <p>It is a Client base project, We use bootstrap javascript
+                                and jquery for front-end. Php language use in back-end.
+                                Phpmyadmin Xamp local server.All Crud functions used
+                                in it.</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="text">
+                            <h4>Manage Khata</h4>
+                            <p>User can manage all the record of his expenses,
+                                investment, income, lending and borrowing the amount.
+                                in which i have used booststrap, Javascript, Php, Jquery,
+                                Mysql etc and this website is on working</p>
+                        </div>
                     </div>
                 </div>
-                <div class="box">
-
-                    <div class="text">
-                        <h4>Self Employed</h4>
-                        <p>I have completed some projects on my own. As like Web Development project, after that i am
-                            also participated in the Final Year Project which we are completed as a team, and some other
-                            projects as well which are add in the projects menu already.</p>
+            
+                <div class="about skills">
+                    <h2 class="title2">Professional Skills</h2>
+                    <div class="box">
+                        <h4>Html5</h4>
+                        <div class="percent">
+                            <div style="width: 100%;"></div>
+                        </div>
                     </div>
-                </div>
-               
-             
-                <h2 class="title2">Personal Projects</h2>
-                <div class="box">
-                    <div class="text">
-                        <h4>CS Software Providere </h4>
-                        <p>It is a Client base project I used Hml.CSS.Js front-end. Php language use in back-end.
-                            Phpmyadmin Xamp local server. All Crud functions used in it.</p>
+                    <div class="box">
+                        <h4>CSS3</h4>
+                        <div class="percent">
+                            <div style="width: 90%;"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="box">
-                    <div class="text">
-                        <h4>Inventory Managment System (FYP)</h4>
-                        <p>It is a Client base project, We use bootstrap javascript
-                            and jquery for front-end. Php language use in back-end.
-                            Phpmyadmin Xamp local server.All Crud functions used
-                            in it.</p>
+                    <div class="box">
+                        <h4>JavaScript</h4>
+                        <div class="percent">
+                            <div style="width: 80%;"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="box">
-
-                    <div class="text">
-                        <h4>Manage Khata</h4>
-                        <p>User can manage all the record of his expenses,
-                            investment, income, lending and borrowing the amount.
-                            in which i have used booststrap, Javascript, Php, Jquery,
-                            Mysql etc and this website is on working</p>
+                    <div class="box">
+                        <h4>PHP</h4>
+                        <div class="percent">
+                            <div style="width: 70%;"></div>
+                        </div>
                     </div>
-                </div>
-
-            </div>
-        
-        <div class="about skills">
-            <h2 class="title2">Professional Skills</h2>
-            <div class="box">
-                <h4>Html5</h4>
-                <div class="percent">
-                    <div style="width: 100%;"></div>
-                </div>
-            </div>
-            <div class="box">
-                <h4>CSS3</h4>
-                <div class="percent">
-                    <div style="width: 90%;"></div>
-                </div>
-            </div>
-            <div class="box">
-                <h4>JavaScript</h4>
-                <div class="percent">
-                    <div style="width: 80%;"></div>
-                </div>
-            </div>
-            <div class="box">
-                <h4>PHP</h4>
-                <div class="percent">
-                    <div style="width: 70%;"></div>
-                </div>
-            </div>
-            <div class="box">
-                <h4>Word Press</h4>
-                <div class="percent">
-                    <div style="width: 80%;"></div>
-                </div>
-            </div>
-            <div class="box">
-                <h4>My SQL</h4>
-                <div class="percent">
-                    <div style="width: 90%;"></div>
-                </div>
-            </div>
-            <div class="box">
-                <h4>Ajax</h4>
-                <div class="percent">
-                    <div style="width: 60%;"></div>
-                </div>
-            </div>
-            <div class="box">
-                <h4>Jquery</h4>
-                <div class="percent">
-                    <div style="width: 70%;"></div>
+                    <div class="box">
+                        <h4>Word Press</h4>
+                        <div class="percent">
+                            <div style="width: 80%;"></div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <h4>My SQL</h4>
+                        <div class="percent">
+                            <div style="width: 90%;"></div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <h4>Ajax</h4>
+                        <div class="percent">
+                            <div style="width: 60%;"></div>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <h4>Jquery</h4>
+                        <div class="percent">
+                            <div style="width: 70%;"></div>
+                        </div>
+                    </div>
+                    
+                    <div class="about interest">
+                        <h2 class="title2">Interest</h2>
+                        <ul>
+                            <li><i class="fa fa-trophy" aria-hidden="true"></i>Cricket</li>
+                            <li><i class="fa fa-code" aria-hidden="true"></i>Development</li>
+                            <li><i class="fa fa-tree" aria-hidden="true"></i>Gardening</li>
+                            <li><i class="fa fa-heart" aria-hidden="true"></i>Physical Activities</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-       
-        <div class="about interest">
-            <h2 class="title2">Interest</h2>
-            <ul>
-                <li><i class="fa fa-trophy" aria-hidden="true"></i>Cricket</li>
-                <li><i class="fa fa-code" aria-hidden="true"></i>Development</li>
-                <li><i class="fa fa-tree" aria-hidden="true"></i>Gardening</li>
-                <li><i class="fa fa-heart" aria-hidden="true"></i>Physical Activities</li>
-            </ul>
-        </div>
-        </div>
-    </div>
     </div>
 </body>
 
